82dd92f660a88a29caaee7cf96cf0b3064951bcf
fuad <nafisfuadisc@gmail.com>
2025-01-14 01:57:09 +0600

    Database Service & AppBar Action Works

--- lib/pages/auth/root/nagivation_pages/home_page/services/codeforces/appbar_action.dart
+++ lib/pages/auth/root/nagivation_pages/home_page/services/codeforces/appbar_action.dart
@@ -2,18 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:student_app/pages/auth/ui_components/my_button.dart';
 import 'package:student_app/pages/auth/ui_components/my_textfield.dart';
 import 'package:student_app/pages/auth/root/nagivation_pages/home_page/services/codeforces/codeforces.dart';
+import 'package:student_app/pages/auth/root/nagivation_pages/home_page/services/codeforces/navigation_pages/api/database_service.dart';
 
-class AppbarAction extends StatelessWidget {
-  final TextEditingController _handlerName = TextEditingController();
+class AppbarAction extends StatelessWidget {  
+  final DatabaseService _database = DatabaseService();
+  final TextEditingController _handleController = TextEditingController();
 
   AppbarAction({super.key});
 
-  void pushHandle(BuildContext context, String handle) {
+  void setName(BuildContext context) async {
+    await _database.setHandle(_handleController.text);    
+    // ignore: use_build_context_synchronously
+    changeRoute(context);
+  }
+
+  void changeRoute(BuildContext context) {
     FocusScope.of(context).unfocus();
 
     Navigator.pop(context);
     Navigator.pop(context);
-    Navigator.push(context, MaterialPageRoute(builder: (context) => Codeforces(handle: handle)));
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const Codeforces(handle: "",)));
   }
 
   @override
@@ -29,38 +37,34 @@ class AppbarAction extends StatelessWidget {
         elevation: 1.0,
       ),
 
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 64.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Set up handle here",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.0,
-                    letterSpacing: 1.5,
-                  )
-                ),
-            
-                const SizedBox(height: 15.0),
-            
-                MyTextfield(
-                  controller: _handlerName,
-                  hintText: "handle name",
-                  obscureText: false
-                ),
-            
-                const SizedBox(height: 15.0),
-            
-                MyButton(
-                  text: "Set Handle",
-                  onTap: () => pushHandle(context, _handlerName.text),
-                ),
-              ],
-            ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 64.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Set up handle here",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  letterSpacing: 1.5,
+                )
+              ),
+              const SizedBox(height: 15.0),
+
+              MyTextfield(
+                controller: _handleController,
+                hintText: "handle name",
+                obscureText: false
+              ),
+              const SizedBox(height: 15.0),
+
+              MyButton(
+                text: "S  E  T",
+                onTap: () => setName(context),
+              ),
+            ],
           ),
         ),
       ),
---
+++ lib/pages/auth/root/nagivation_pages/home_page/services/codeforces/navigation_pages/api/database_service.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class DatabaseService {
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> setHandle(String handle) async {
+    final String currentUserID = _firebaseAuth.currentUser!.uid;
+
+    Map<String, dynamic> handleMap = {
+      "handle" : handle,
+    };
+
+    await _firestore
+      .collection("codeforces_accounts")
+      .doc(currentUserID)
+      .set(handleMap);
+  }
+
+  Future<String?> fetchHandle(String uid) async {
+    DocumentReference docRef = _firestore.collection("codeforces_accounts").doc(uid);
+    
+    
+
+    try {
+      DocumentSnapshot docSnapshot = await docRef.get();
+
+      if (docSnapshot.exists) {
+        return docSnapshot.get("handle") as String?;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print("Error fetching handle: $e"); // ignore: avoid_print
+      return null;
+    }
+  }
+}
